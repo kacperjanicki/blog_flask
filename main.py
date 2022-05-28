bce005fa5905d4170e46817aa80f9f86d69d545a
kacperjanicki <kacperekjanicki9@gamil.com>
2022-05-28 21:45:37 +0200

    admin can now view posts, but edit button is not working as intended (error appears)

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_ckeditor import CKEditor
+from flask import request
 from datetime import date
 from functools import wraps
 from flask import abort
@@ -105,9 +106,9 @@ def logout():
     return redirect('/')
 
 
-@app.route("/post/<int:post_id>")
-def show_post(post_id):
-    requested_post = BlogPost.query.get(post_id)
+@app.route("/post")
+def show_post():
+    requested_post = BlogPost.query.get(request.args.get('post_id'))
     print(requested_post)
     return render_template("post.html", post=requested_post)
 
@@ -139,22 +140,22 @@ def add_new_post():
 
 
 
-@app.route("/edit-post/<int:post_id>")
-@admin_only
-def edit_post(post_id):
+@app.route("/edit-post",methods=['POST','GET'])
+# @admin_only
+def edit_post():
+    post_id=request.args.get('post_id')
     post = BlogPost.query.get(post_id)
     edit_form = CreatePostForm(
         title=post.title,
         subtitle=post.subtitle,
         img_url=post.img_url,
-        author=current_user.author,
         body=post.body
     )
     if edit_form.validate_on_submit():
         post.title = edit_form.title.data
         post.subtitle = edit_form.subtitle.data
         post.img_url = edit_form.img_url.data
-        post.author = edit_form.author.data
+        post.author = current_user
         post.body = edit_form.body.data
         db.session.commit()
         return redirect(url_for("show_post", post_id=post.id))
